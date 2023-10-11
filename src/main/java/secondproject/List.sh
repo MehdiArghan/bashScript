f54ed4618d7e23ed431edd904342addb71929c7a
mehdi <mehdiarghan@gmail.com.git/>
2023-10-12 01:26:37 +0330

    Changes in some parts of the List.sh file

--- src/main/java/secondproject/List.sh
+++ src/main/java/secondproject/List.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 while [ true ]; do
-  select option in create print replace; do
+  select option in create added replace; do
     case $option in
     "create")
       for i in {1..5}; do
         echo >>"File${i}.txt"
       done
       ;;
-    "print")
+    "added")
       for i in {1..5}; do
         echo "Hello World" >>"File${i}.txt"
       done
       ;;
     "replace")
+    read -p "Enter the search string: " search
+    read -p "Enter the replace string: " replace
       for i in {1..5}; do
-        sed 's/World/Bash/g' File${i}.txt | cat >File${i}.txt
+       sed -i "s/$search/$replace/" File"${i}".txt
       done
       ;;
     esac
